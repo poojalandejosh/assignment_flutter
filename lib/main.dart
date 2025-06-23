c624a121ccd34f26a1924cf9e3eb13aa5f67ddd5
Pooja lande <Pooja.lande.-nd@disney.com>
2025-06-23 16:02:52 +0530

    Initial commit: Flutter project setup

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); // ← pass MyApp here
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      home: Main()
+      
+    );
+  }
+}
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello"),
+    );
+  }
+}
+
